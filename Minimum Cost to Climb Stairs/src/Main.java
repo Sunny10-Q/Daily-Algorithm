b33c46580c8397a6ae7197dee0344fb925c88860
晴曛 <3316361825@qq.com>
2026-08-24 17:56:12 +0800

    leetcode——LCR.088.使用最小花费爬楼梯（动态规划）

---
+++ Minimum Cost to Climb Stairs/src/Main.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        int[]dp=new int[n+1];
+
+        for(int i=2;i<n+1;i++){
+            dp[i]=Math.min(dp[i-2]+cost[i-2],dp[i-1]+cost[i-1]);
+        }
+        return dp[n];
+
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("leetcode——LCR.088.使用最小花费爬楼梯（动态规划）");
+    }
+}
